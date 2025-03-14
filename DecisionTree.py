375eeb6c792d917bb0d6f89bf28da91eb149b075
hexa <dianatikian@gmail.com>
2025-03-14 16:02:14 -0400

    Decision trees

---
+++ DecisionTree.py
@@ -0,0 +1,91 @@
+import numpy as np
+import pandas as pd
+import math
+
+def _entropy(L):
+    items, counts = np.unique(L, return_counts=True)
+    probs = counts / counts.sum()
+    return -sum(p * np.log2(p) for p in probs)
+
+def _info_gain(data, features, target, test):
+    H_X = _entropy(data[target].values)
+    feature, threshold = test.feature, test.threshold
+    
+    left_split = data[data[feature] > threshold]
+    right_split = data[data[feature] <= threshold]
+
+    n, nL, nR = len(data), len(left_split), len(right_split)
+    
+    if nL == 0 or nR == 0:
+        return 0
+
+    H_XL = _entropy(left_split[target].values)
+    H_XR = _entropy(right_split[target].values)
+    
+    IG = H_X - (nL / n) * H_XL - (nR / n) * H_XR
+    return IG
+
+class DecisionTest:
+    def __init__(self, feature, threshold):
+        self.feature = feature
+        self.threshold = threshold
+        
+    def __call__(self, x):
+        return x[self.feature] > self.threshold
+
+
+class DecisionTree:
+    def __init__(self, data, features, target, left=None, right=None, test=None):
+        self.data = data
+        self.features = data[features]
+        self.target = data[target]
+        self.target_label = target
+        self.feature_labels = features
+        
+        self.left = left
+        self.right = right
+        self.test = test
+        
+    def terminal(self):
+        return not self.left or not self.right
+    
+    def predict(self, x, debug=False, indent=0):
+        if self.terminal():
+            if debug: print(indent * ' ' + 'terminal node')
+            classes, counts = np.unique(self.target, return_counts=True)
+            return classes[np.argmax(counts)]
+        if debug:
+            print(indent * ' ', self.right, self.test, self.left)
+        return self.right.predict(x, debug=debug, indent=indent+2) if self.test(x) else self.left.predict(x, debug=debug, indent=indent+2)
+    
+    def learn(self, depth=0, debug=False, max_depth=math.inf):
+        max_info_gain = 0
+        optimal_test = None
+        
+        for feature in self.feature_labels:
+            observations = np.unique(self.features[feature])
+            for i in range(len(observations) - 1):
+                threshold = (observations[i] + observations[i+1])/2
+                test = DecisionTest(feature, threshold)
+                info_gain = _info_gain(self.data, self.feature_labels, self.target_label, test)
+                
+                if info_gain > max_info_gain:
+                    optimal_test = test
+                    max_info_gain = info_gain
+                    
+        if debug and optimal_test:
+            print(2*depth * ' ' + f'{optimal_test.feature} > {optimal_test.threshold}, {max_info_gain}')
+                    
+        if depth >= max_depth or not optimal_test:
+            if debug: print(2*depth * ' ' + f'Test: leaf')
+            return
+        
+        if debug:
+            print(2*depth * ' ' + f'Test: {optimal_test.feature} > {optimal_test.threshold}')
+        
+        self.test = optimal_test
+        self.left = DecisionTree(self.data[self.data[optimal_test.feature] <= optimal_test.threshold], self.feature_labels, self.target_label)
+        self.right = DecisionTree(self.data[self.data[optimal_test.feature] > optimal_test.threshold], self.feature_labels, self.target_label)
+        
+        self.left.learn(debug=debug, depth=depth + 1, max_depth=max_depth)
+        self.right.learn(debug=debug, depth=depth + 1, max_depth=max_depth)
